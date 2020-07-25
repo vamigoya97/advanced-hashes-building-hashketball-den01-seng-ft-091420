edc3e0f8579a17645cd5dcd0f28738aabe1d67bd
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-25 04:22:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,15 +29,7 @@ def game_hash
           :slam_dunks => 
         }
           
-        "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"],
-        :number => [0, 30, 11, 1, 31],
-        :shoe => [16, 14, 17, 19, 15],
-        :points => [22, 12, 17, 26, 19],
-        :rebounds => [12, 12, 19, 11, 2],
-        :assists => [12, 12, 10, 6, 2],
-        :steals => [3, 12, 3, 3, 4],
-        :blocks => [1, 12, 1, 8, 11],
-        :slam_dunks => [1, 7, 15, 5, 1]
+       
       }
     },
     :away => {
