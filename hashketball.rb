28e410ef6a323cb3df51a7d1693f115d1f726102
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-25 04:25:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,29 +29,29 @@ def game_hash
           :slam_dunks => 7 
         }
         {
-          :player_name => "Reggie Evans",
-          :number => 
-          :shoe => 
-          :points => 
-          :rebounds => 
-          :assists => 
-          :steals => 
-          :blocks => 
-          :slam_dunks => 
+          :player_name => "Brook Lopez",
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15 
         }
         {
-          :player_name => "Reggie Evans",
-          :number => 
-          :shoe => 
-          :points => 
-          :rebounds => 
-          :assists => 
-          :steals => 
-          :blocks => 
-          :slam_dunks => 
+          :player_name => "Mason Plumlee",
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 11,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5 
         }
         {
-          :player_name => "Reggie Evans",
+          :player_name => "Jason Terry",
           :number => 
           :shoe => 
           :points => 
