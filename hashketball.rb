fdea57a6df3199aae6a8225fac5c1afd60ce5dca
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-25 04:24:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,14 +19,14 @@ def game_hash
         }
         {
           :player_name => "Reggie Evans",
-          :number => 
-          :shoe => 
-          :points => 
-          :rebounds => 
-          :assists => 
-          :steals => 
-          :blocks => 
-          :slam_dunks => 
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7 
         }
         {
           :player_name => "Reggie Evans",
@@ -67,16 +67,61 @@ def game_hash
       :team_name => ["Charlotte Hornets"],
       :colors => ["Turquoise", "Purple"],
       :players => {
-        :player_name => ["Jeff Adrien", "Bismack Biyombo", "DeSagna Diop", "Ben Gordon", "Kemba Walker"],
-        :number => [4, 0, 2, 8, 33],
-        :shoe => [18, 16, 14, 15, 15],
-        :points => [10, 12, 24, 33, 6],
-        :rebounds => [1, 4, 12, 3, 12],
-        :assists => [1, 7, 12, 2, 12],
-        :steals => [2, 22, 4, 1, 7],
-        :blocks => [7, 15, 5, 1, 5],
-        :slam_dunks => [2, 10, 5, 0, 12]
-     }
+        {
+          :player_name => "Reggie Evans",
+          :number => 
+          :shoe => 
+          :points => 
+          :rebounds => 
+          :assists => 
+          :steals => 
+          :blocks => 
+          :slam_dunks => 
+        }
+        {
+          :player_name => "Reggie Evans",
+          :number => 
+          :shoe => 
+          :points => 
+          :rebounds => 
+          :assists => 
+          :steals => 
+          :blocks => 
+          :slam_dunks => 
+        }
+        {
+          :player_name => "Reggie Evans",
+          :number => 
+          :shoe => 
+          :points => 
+          :rebounds => 
+          :assists => 
+          :steals => 
+          :blocks => 
+          :slam_dunks => 
+        }
+        {
+          :player_name => "Reggie Evans",
+          :number => 
+          :shoe => 
+          :points => 
+          :rebounds => 
+          :assists => 
+          :steals => 
+          :blocks => 
+          :slam_dunks => 
+        }
+        {
+          :player_name => "Reggie Evans",
+          :number => 
+          :shoe => 
+          :points => 
+          :rebounds => 
+          :assists => 
+          :steals => 
+          :blocks => 
+          :slam_dunks => 
+        }
     }
   }
   return game_hash
