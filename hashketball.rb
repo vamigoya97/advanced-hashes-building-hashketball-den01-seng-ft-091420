576836648e6d4a13ba9a9f9c4abd98437a2c02cc
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-25 04:27:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,14 +52,14 @@ def game_hash
         }
         {
           :player_name => "Jason Terry",
-          :number => 
-          :shoe => 
-          :points => 
-          :rebounds => 
-          :assists => 
-          :steals => 
-          :blocks => 
-          :slam_dunks => 
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1  
         }
       }
     },
@@ -68,26 +68,26 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => {
         {
-          :player_name => "Reggie Evans",
-          :number => 
-          :shoe => 
-          :points => 
-          :rebounds => 
-          :assists => 
-          :steals => 
-          :blocks => 
-          :slam_dunks => 
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2  
         }
         {
-          :player_name => "Reggie Evans",
-          :number => 
-          :shoe => 
-          :points => 
-          :rebounds => 
-          :assists => 
-          :steals => 
-          :blocks => 
-          :slam_dunks => 
+          :player_name => "Bismack Biyombo",
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10  
         }
         {
           :player_name => "Reggie Evans",
